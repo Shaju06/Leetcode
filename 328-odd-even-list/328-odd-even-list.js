9d4c6df2ee386147459e656a671129dc06da3557
varinderpal18f <vasingh@1800flowers.com>
2022-07-17 11:25:53 +0530

    LET-328: odd even list

---
+++ 328-odd-even-list/328-odd-even-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var oddEvenList = function(head) {
+    if(!head || !head.next) return head
+    let slow = head
+    let fast = head.next
+    while(fast && fast.next) {
+        const add = fast.next
+        fast.next = add.next
+        fast = fast.next
+        console.log(add, fast)
+        add.next = slow.next
+        slow.next = add
+        slow = slow.next
+    }
+    return head
+};
