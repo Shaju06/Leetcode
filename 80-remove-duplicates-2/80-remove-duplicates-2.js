6c125bb3b435d251d5898826ed34a582f4204042
Varinder <Varipaul@gmail.com>
2023-06-03 21:02:46 +0530

    pushing another algo solution

---
+++ 80-remove-duplicates-2/80-remove-duplicates-2.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0;
+for (let num of nums) {
+  if (i < 2 || num > nums[i - 2]) {
+    nums[i] = num;
+    i++;
+  }
+}
+nums.splice(i);
+return nums.length;
+};
